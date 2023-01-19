fec46e4b46bce1186d8f2586d2208c7c48a1035f
jheer <jeffrey.heer@gmail.com>
2023-01-18 16:16:28 -0800

    fix: Update bin directive to be mark/channel sensitive.

--- src/vgplot/directives/data.js
+++ src/vgplot/directives/data.js
@@ -5,8 +5,22 @@ export function from(table, options) {
 }
 
 export function bin(field, options = { steps: 25 }) {
-  return channel => ({
-    [`${channel}1`]: binTransform(field, options),
-    [`${channel}2`]: binTransform(field, { ...options, offset: 1 })
-  });
+  return (channel, type) => {
+    return hasExtent(channel, type)
+      ? {
+          [`${channel}1`]: binTransform(field, options),
+          [`${channel}2`]: binTransform(field, { ...options, offset: 1 })
+        }
+      : {
+          [channel]: binTransform(field, options)
+        };
+  };
+}
+
+const EXTENT = [
+  'rectY-x', 'rectX-y', 'rect-x', 'rect-y'
+];
+
+function hasExtent(channel, type) {
+  return EXTENT.includes(`${type}-${channel}`);
 }
