c96f8f32882666e5b097142927566c1af0f87f92
jheer <jeffrey.heer@gmail.com>
2023-01-21 09:16:14 -0800

    feat: Add spacing plot directives.

--- src/vgplot/directives/plots.js
+++ src/vgplot/directives/plots.js
@@ -57,3 +57,20 @@ export function hconcat(...plots) {
     { type: 'horizontal' }
   ).element;
 }
+
+function space(dim = 'width', size = 10) {
+  const span = document.createElement('span');
+  span.style.display = 'inline-block';
+  span.style[dim] = Number.isNaN(+size) ? size : `${size}px`;
+  const obj = { element: span };
+  span.value = obj;
+  return span;
+}
+
+export function vspace(size) {
+  return space('height', size);
+}
+
+export function hspace(size) {
+  return space('width', size);
+}
