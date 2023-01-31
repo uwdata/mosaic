105ab09e008b034fe82604b0e006deb2ca7db2bb
jheer <jeffrey.heer@gmail.com>
2023-01-30 17:59:00 -0800

    perf: Add initial benchmarks.

---
+++ dev/benchmarks/benchmark-utils.js
@@ -0,0 +1,121 @@
+const { plot } = vgplot;
+
+let abort;
+let results;
+let renderCount;
+let renderDone;
+
+export function watchRender(count, done) {
+  renderCount = count;
+  renderDone = done;
+}
+
+export function mplot(...args) {
+  const p = plot(...args);
+  const oldRender = p.value.render;
+  p.value.render = async function() {
+    await oldRender.call(p.value);
+    performance.mark('plot-render');
+    onRender(p);
+  };
+  return p;
+}
+
+function onRender() {
+  if (renderDone) {
+    if (--renderCount === 0) {
+      const done = renderDone;
+      renderDone = null;
+      done();
+    }
+  }
+}
+
+export async function run(tasks) {
+  measureInit();
+  for (const task of tasks) {
+    if (!abort) await task();
+  }
+  console.log('INIT', stats(results.filter(x => x.type === 'init')));
+  console.log('UPDATE', stats(results.filter(x => x.type === 'update')));
+  return (self.results = results);
+}
+
+export function stop() {
+  abort = true;
+}
+
+export function startInit() {
+  abort = false;
+  results = [];
+  performance.mark('benchmark-init');
+}
+
+export function startUpdate() {
+  performance.mark('benchmark-update');
+}
+
+export function measureInit(props = {}) {
+  const p = performance.measure('init-time', 'benchmark-init', 'plot-render');
+  results.push({ type: 'init', time: p.duration, ...props });
+}
+
+export function measureUpdate(props = {}) {
+  const p = performance.measure('update-time', 'benchmark-update', 'plot-render');
+  results.push({ type: 'update', time: p.duration, ...props });
+}
+
+export function brushes1d(interval, renderCount) {
+  const updater = (range, props) => () => new Promise(resolve => {
+    watchRender(renderCount, () => { resolve(measureUpdate(props)); });
+    startUpdate();
+    interval.publish(range);
+  });
+  const tasks = [];
+  const { range } = interval.scale;
+  const [lo, hi] = range;
+  const extent = hi - lo;
+  [0.05, 0.10, 0.20].forEach(perc => {
+    const w = Math.round(perc * extent);
+    for (let index = 0; index < extent - w; ++index) {
+      tasks.push(updater([lo + index, lo + index + w], { perc, index }));
+    }
+    tasks.push(updater(null, { perc: 1, index: -1 })); // clear
+  });
+  return tasks;
+}
+
+export function stats(results) {
+  const t = Float64Array.from(results.map(r => r.time));
+  const n = t.length;
+  t.sort((a, b) => a - b);
+  let sum = 0;
+  for (let i = 0; i < n; ++i) {
+    sum += t[i];
+  }
+  return {
+    fps: 1000 * (n / sum),
+    avg: sum / n,
+    min: t[0],
+    q01: quantileSorted(t, 0.01),
+    q05: quantileSorted(t, 0.05),
+    q25: quantileSorted(t, 0.25),
+    q50: quantileSorted(t, 0.50),
+    q75: quantileSorted(t, 0.75),
+    q95: quantileSorted(t, 0.95),
+    q99: quantileSorted(t, 0.99),
+    max: t[t.length - 1]
+  };
+}
+
+function quantileSorted(values, p) {
+  if (!(n = values.length) || isNaN(p = +p)) return;
+  if (p <= 0 || n < 2) return values[0];
+  if (p >= 1) return values[n - 1];
+  var n,
+      i = (n - 1) * p,
+      i0 = Math.floor(i),
+      value0 = values[i0],
+      value1 = values[i0 + 1];
+  return value0 + (value1 - value0) * (i - i0);
+}
---
+++ dev/benchmarks/flights-10m.js
@@ -0,0 +1,68 @@
+import {
+  mplot, watchRender, startInit, brushes1d, run
+} from './benchmark-utils.js';
+
+const {
+  Query, Selection, Fixed, coordinator, namedPlots, vconcat, from, name,
+  bin, count, expr, rectY, width, height, domainX, intervalX
+} = vgplot;
+
+export default async function(el) {
+  // Load 10M flights data from the web, as needed
+  const q = Query
+    .select({
+      delay: expr('GREATEST(-60, LEAST(ARR_DELAY, 180))::DOUBLE'),
+      distance: 'DISTANCE',
+      time: 'DEP_TIME'
+    })
+    .from(expr(`'https://vega.github.io/falcon/flights-10m.becad501.parquet'`));
+  await coordinator().exec(`CREATE TABLE IF NOT EXISTS faa AS ${q}`);
+
+  watchRender(3, () => {
+    const tasks = ['delay', 'time', 'distance']
+      .map(x => namedPlots.get(x))
+      .flatMap(plot => brushes1d(plot.selections[0], 2));
+    run(tasks);
+  });
+  startInit();
+
+  const table = 'faa';
+  const brush = Selection.crossfilter();
+  el.appendChild(
+    vconcat(
+      mplot(
+        name('delay'),
+        rectY(
+          from(table, { filterBy: brush }),
+          { x: bin('delay'), y: count(), fill: 'steelblue', inset: 0.5 }
+        ),
+        intervalX({ as: brush }),
+        domainX(Fixed),
+        width(600),
+        height(200)
+      ),
+      mplot(
+        name('time'),
+        rectY(
+          from(table, { filterBy: brush }),
+          { x: bin('time'), y: count(), fill: 'steelblue', inset: 0.5 }
+        ),
+        intervalX({ as: brush }),
+        domainX(Fixed),
+        width(600),
+        height(200)
+      ),
+      mplot(
+        name('distance'),
+        rectY(
+          from(table, { filterBy: brush }),
+          { x: bin('distance'), y: count(), fill: 'steelblue', inset: 0.5 }
+        ),
+        intervalX({ as: brush }),
+        domainX(Fixed),
+        width(600),
+        height(200)
+      )
+    )
+  );
+}
