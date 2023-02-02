0ffee605e0f024bf3d17973c8139bac7eb7948a1
jheer <jeffrey.heer@gmail.com>
2023-02-02 10:57:49 -0800

    feat: Add sqlFrom utility.

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,6 +2,7 @@ export { MosaicClient } from './MosaicClient.js';
 export { Coordinator, coordinator } from './Coordinator.js';
 export { Selection, isSelection } from './Selection.js';
 export { Signal, isSignal } from './Signal.js';
+export { sqlFrom } from './util/sql-from.js';
 export { throttle } from './util/throttle.js';
 export { restClient } from './clients/rest.js';
 export { socketClient } from './clients/socket.js';
---
+++ packages/core/src/util/sql-from.js
@@ -0,0 +1,22 @@
+import { literalToSQL } from '@mosaic/sql';
+
+export function sqlFrom(data, {
+  columns = Object.keys(data?.[0] || {})
+} = {}) {
+  let keys = [];
+  if (Array.isArray(columns)) {
+    keys = columns;
+    columns = keys.reduce((m, k) => (m[k] = k, m), {});
+  } else if (columns) {
+    keys = Object.keys(columns);
+  }
+  if (!keys.length) {
+    throw new Error('Can not create table from empty column set.');
+  }
+  const subq = [];
+  for (const datum of data) {
+    const sel = keys.map(k => `${literalToSQL(datum[k])} AS "${columns[k]}"`);
+    subq.push(`(SELECT ${sel.join(', ')})`);
+  }
+  return subq.join(' UNION ALL ');
+}
