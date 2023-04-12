b5e190e951ee45012ec18edfb323eb86afb46cd5
jheer <jeffrey.heer@gmail.com>
2023-04-12 14:28:19 -0700

    feat: Update selection helper constructors, add jsdoc.

--- packages/core/src/util/AsyncDispatch.js
+++ packages/core/src/util/AsyncDispatch.js
@@ -1,8 +1,25 @@
+/**
+ * Event dispatcher supporting asynchronous updates.
+ * If an event handler callback returns a Promise, this dispatcher will
+ * wait for all such Promises to settle before dispatching future events
+ * of the same type.
+ */
 export class AsyncDispatch {
+
+  /**
+   * Create a new asynchronous dispatcher instance.
+   */
   constructor() {
     this._callbacks = new Map;
   }
 
+  /**
+   * Add an event listener callback for the provided event type.
+   * @param {string} type The event type.
+   * @param {(value: *) => Promise?} callback The event handler
+   *  callback function to add. If the callback has already been
+   *  added for the event type, this method has no effect.
+   */
   addEventListener(type, callback) {
     if (!this._callbacks.has(type)) {
       this._callbacks.set(type, {
@@ -15,6 +32,12 @@ export class AsyncDispatch {
     entry.callbacks.add(callback);
   }
 
+  /**
+   * Remove an event listener callback for the provided event type.
+   * @param {string} type The event type.
+   * @param {(value: *) => Promise?} callback The event handler
+   *  callback function to remove.
+   */
   removeEventListener(type, callback) {
     const entry = this._callbacks.get(type);
     if (entry) {
@@ -22,21 +45,52 @@ export class AsyncDispatch {
     }
   }
 
+  /**
+   * Lifecycle method that returns the event value to emit.
+   * This default implementation simply returns the input value as-is.
+   * Subclasses may override this method to implement custom transformations
+   * prior to emitting an event value to all listeners.
+   * @param {string} type The event type.
+   * @param {*} value The event value.
+   * @returns The (possibly transformed) event value to emit.
+   */
   willEmit(type, value) {
-    // pass value as-is, superclasses may override
     return value;
   }
 
+  /**
+   * Lifecycle method that returns a filter function for updating the
+   * queue of unemitted event values prior to enqueueing a new value.
+   * This default implementation simply returns null, indicating that
+   * any other unemitted event values should be dropped (that is, all
+   * queued events are filtered)
+   * @param {*} value The new event value that will be enqueued.
+   * @returns {(value: *) => boolean|null} A dispatch queue filter
+   *  function, or null if all unemitted event values should be filtered.
+   */
   emitQueueFilter() {
     // removes all pending items
     return null;
   }
 
+  /**
+   * Cancel all unemitted event values for the given event type.
+   * @param {string} type The event type.
+   */
   cancel(type) {
     const entry = this._callbacks.get(type);
     entry?.queue.clear();
   }
 
+  /**
+   * Emit an event value to listeners for the given event type.
+   * If a previous emit has not yet resolved, the event value
+   * will be queued to be emitted later.
+   * The actual event value given to listeners will be the result
+   * of passing the input value through the emitValue() method.
+   * @param {string} type The event type.
+   * @param {*} value The event value.
+   */
   emit(type, value) {
     const entry = this._callbacks.get(type) || {};
     if (entry.pending) {
@@ -59,19 +113,44 @@ export class AsyncDispatch {
   }
 }
 
+/**
+ * Queue for managing unemitted event values.
+ */
 export class DispatchQueue {
+
+  /**
+   * Create a new dispatch queue instance.
+   */
   constructor() {
     this.clear();
   }
 
+  /**
+   * Clear the queue state of all event values.
+   */
   clear() {
     this.next = null;
   }
 
+  /**
+   * Indicate if the queue is empty.
+   * @returns {boolean} True if queue is empty, false otherwise.
+   */
   isEmpty() {
     return !this.next;
   }
 
+  /**
+   * Add a new value to the queue, and optionally filter the
+   * current queue content in response.
+   * @param {*} value The value to add.
+   * @param {(value: *) => boolean} [filter] An optional filter
+   *  function to apply to existing queue content. If unspecified
+   *  or falsy, all previously queued values are removed. Otherwise,
+   *  the provided function is applied to all queue entries. The
+   *  entry is retained if the filter function returns a truthy value,
+   *  otherwise the entry is removed.
+   */
   enqueue(value, filter) {
     const tail = { value };
     if (filter && this.next) {
@@ -87,9 +166,12 @@ export class DispatchQueue {
     } else {
       this.next = tail;
     }
-    return this;
   }
 
+  /**
+   * Remove and return the next queued event value.
+   * @returns {*} The next event value in the queue.
+   */
   dequeue() {
     const { next } = this;
     this.next = next?.next;
