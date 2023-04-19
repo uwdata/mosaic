7ffaf38f119e50fe67faed0ac120ad59c8db5d8b
jheer <jeffrey.heer@gmail.com>
2023-04-19 14:49:02 -0700

    test: Add Density2D dot example.

--- dev/examples/density2d.js
+++ dev/examples/density2d.js
@@ -1,32 +1,30 @@
 import * as vg from '../setup.js';
 
-export default function(el) {
-  const {
-    Param, slider, vconcat, hconcat, plot, from,
-    contour, raster, scaleColor, schemeColor,
-    axisX, axisY, labelAnchorX, labelAnchorY, zeroX,
-    marginLeft, marginRight, width, height,
-  } = vg;
-
-  const bandwidth = Param.value(7);
-  const thresholds = Param.value(10);
-  const table = 'flights';
-  const x = 'time';
-  const y = 'delay';
+export default function(el = document.body) {
+  const bandwidth = vg.Param.value(40);
+  const bins = vg.Param.value(10);
+  const table = 'penguins';
+  const x = 'bill_length';
+  const y = 'bill_depth';
 
   el.appendChild(
-    vconcat(
-      hconcat(
-        slider({ label: 'Bandwidth (σ)', as: bandwidth, min: 1, max: 100 }),
-        slider({ label: 'Thresholds', as: thresholds, min: 2, max: 20 })
+    vg.vconcat(
+      vg.hconcat(
+        vg.slider({ label: 'Bandwidth (σ)', as: bandwidth, min: 1, max: 100 }),
+        vg.slider({ label: 'Bins', as: bins, min: 3, max: 40 })
       ),
-      plot(
-        scaleColor('symlog'), schemeColor('ylgnbu'),
-        raster(from(table), { x, y, fill: 'density', bandwidth }),
-        contour(from(table), { x, y, stroke: 'white', strokeOpacity: 0.5, bandwidth, thresholds }),
-        axisX('top'), labelAnchorX('center'), zeroX(true),
-        axisY('right'), labelAnchorY('center'),
-        width(700), height(500), marginLeft(5), marginRight(40)
+      vg.plot(
+        vg.density(vg.from(table), {
+          x, y, r: 'density', fill: '#ddd',
+          binsX: bins, binsY: bins, bandwidth
+        }),
+        vg.dot(vg.from(table), { x, y, fill: 'black', r: 1 }),
+        vg.scaleR('sqrt'), vg.rangeR([0, 16]),
+        vg.axisX('bottom'), vg.labelAnchorX('center'),
+        vg.axisY('right'), vg.labelAnchorY('center'),
+        vg.margins({ top: 5, bottom: 30, left: 5, right: 50 }),
+        vg.width(700),
+        vg.height(480)
       )
     )
   );
