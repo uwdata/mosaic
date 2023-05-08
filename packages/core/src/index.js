d69e71638f07bf8892b8951f0b3921cc6caa534a
jheer <jeffrey.heer@gmail.com>
2023-05-08 16:42:08 -0700

    feat: Expose Priority enum.

--- packages/core/src/index.js
+++ packages/core/src/index.js
@@ -2,6 +2,7 @@ export { MosaicClient } from './MosaicClient.js';
 export { Coordinator, coordinator } from './Coordinator.js';
 export { Selection, isSelection } from './Selection.js';
 export { Param, isParam } from './Param.js';
+export { Priority } from './QueryManager.js';
 export { distinct } from './util/distinct.js';
 export { sqlFrom } from './util/sql-from.js';
 export { synchronizer } from './util/synchronizer.js';
