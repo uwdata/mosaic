393745f98deb0652bf805a62a17aedfb26823c11
jheer <jeffrey.heer@gmail.com>
2023-05-08 17:11:40 -0700

    chore: Query manager clean up.

--- packages/core/src/QueryManager.js
+++ packages/core/src/QueryManager.js
@@ -3,12 +3,11 @@ import { priorityQueue } from './util/priority-queue.js';
 
 export const Priority = { High: 0, Normal: 1, Low: 2 };
 
-export function QueryManager({
-  db,
-  clientCache = lruCache(),
-  logger
-} = {}) {
+export function QueryManager() {
   const queue = priorityQueue(3);
+  let db;
+  let clientCache;
+  let logger;
   let recorders = [];
   let pending = null;
 
@@ -16,42 +15,38 @@ export function QueryManager({
     if (pending || queue.isEmpty()) return;
     const { request, result } = queue.next();
     pending = submit(request, result);
-    pending.finally(() => {
-      pending = null;
-      next();
-    });
+    pending.finally(() => { pending = null; next(); });
   }
 
   async function submit(request, result) {
-    const {
-      query,
-      type,
-      cache = false,
-      options
-    } = request;
-    const sql = query ? String(query) : null;
+    try {
+      const { query, type, cache = false, options } = request;
+      const sql = query ? String(query) : null;
 
-    // record request
-    updateRecorders(recorders, sql);
-
-    // check query cache
-    if (cache) {
-      const cached = clientCache.get(sql);
-      if (cached) {
-        logger.debug('Cache');
-        result.fulfill(cached);
-        return;
+      // update recorders
+      if (recorders.length && sql) {
+        recorders.forEach(rec => rec.add(sql));
       }
+
+      // check query cache
+      if (cache) {
+        const cached = clientCache.get(sql);
+        if (cached) {
+          logger.debug('Cache');
+          result.fulfill(cached);
+          return;
+        }
+      }
+
+      // issue query, potentially cache result
+      const t0 = performance.now();
+      const data = await db.query({ type, sql, ...options });
+      if (cache) clientCache.set(sql, data);
+      logger.debug(`Request: ${(performance.now() - t0).toFixed(1)}`);
+      result.fulfill(data);
+    } catch (err) {
+      result.error(err);
     }
-
-    // issue query, potentially cache result
-    const t0 = performance.now();
-    const data = await db.query({ type, sql, ...options });
-    if (cache) clientCache.set(sql, data);
-    logger.debug(`Request: ${(performance.now() - t0).toFixed(1)}`);
-
-    // return query result
-    result.fulfill(data);
   }
 
   return {
@@ -84,11 +79,17 @@ export function QueryManager({
     record() {
       let state = [];
       const recorder = {
-        add(query) { state.push(query); },
-        reset() { state = []; },
-        snapshot() { return state.slice(); },
+        add(query) {
+          state.push(query);
+        },
+        reset() {
+          state = [];
+        },
+        snapshot() {
+          return state.slice();
+        },
         stop() {
-          recorders = recorders.filter(x => x !== this);
+          recorders = recorders.filter(x => x !== recorder);
           return state;
         }
       };
@@ -96,12 +97,6 @@ export function QueryManager({
       return recorder;
     }
   };
-}
-
-function updateRecorders(recorders, sql) {
-  if (recorders.length && sql) {
-    recorders.forEach(rec => rec.add(sql));
-  }
 }
 
 function observer() {
