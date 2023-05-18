187fc22efe3177cccc2413bf09fbb680384dcebe
Jeffrey Heer <jeffrey.heer@gmail.com>
2023-05-17 22:20:03 -0700

    feat: Add windowed aggr functions, add window functions to JSON spec parser. (#89)

--- packages/sql/test/cast-test.js
+++ packages/sql/test/cast-test.js
@@ -1,38 +1,38 @@
 import assert from 'node:assert';
-import { cast, castDouble, castInteger, column, sum } from '../src/index.js';
+import { avg, cast, castDouble, castInteger, column } from '../src/index.js';
 
 describe('cast', () => {
   it('performs type casts', () => {
     assert.strictEqual(String(cast('foo', 'DOUBLE')), 'CAST("foo" AS DOUBLE)');
     assert.strictEqual(String(cast(column('foo'), 'DOUBLE')), 'CAST("foo" AS DOUBLE)');
 
-    const expr = cast(sum('bar'), 'DOUBLE');
-    assert.strictEqual(String(expr), 'CAST(SUM("bar") AS DOUBLE)');
-    assert.strictEqual(expr.aggregate, 'SUM');
+    const expr = cast(avg('bar'), 'DOUBLE');
+    assert.strictEqual(String(expr), 'CAST(AVG("bar") AS DOUBLE)');
+    assert.strictEqual(expr.aggregate, 'AVG');
     assert.strictEqual(expr.column, 'bar');
     assert.deepStrictEqual(expr.columns, ['bar']);
-    assert.strictEqual(expr.label, 'sum(bar)');
+    assert.strictEqual(expr.label, 'avg(bar)');
   });
   it('performs double casts', () => {
     assert.strictEqual(String(castDouble('foo')), 'CAST("foo" AS DOUBLE)');
     assert.strictEqual(String(castDouble(column('foo'))), 'CAST("foo" AS DOUBLE)');
 
-    const expr = castDouble(sum('bar'));
-    assert.strictEqual(String(expr), 'CAST(SUM("bar") AS DOUBLE)');
-    assert.strictEqual(expr.aggregate, 'SUM');
+    const expr = castDouble(avg('bar'));
+    assert.strictEqual(String(expr), 'CAST(AVG("bar") AS DOUBLE)');
+    assert.strictEqual(expr.aggregate, 'AVG');
     assert.strictEqual(expr.column, 'bar');
     assert.deepStrictEqual(expr.columns, ['bar']);
-    assert.strictEqual(expr.label, 'sum(bar)');
+    assert.strictEqual(expr.label, 'avg(bar)');
   });
   it('performs integer casts', () => {
     assert.strictEqual(String(castInteger('foo')), 'CAST("foo" AS INTEGER)');
     assert.strictEqual(String(castInteger(column('foo'))), 'CAST("foo" AS INTEGER)');
 
-    const expr = castInteger(sum('bar'));
-    assert.strictEqual(String(expr), 'CAST(SUM("bar") AS INTEGER)');
-    assert.strictEqual(expr.aggregate, 'SUM');
+    const expr = castInteger(avg('bar'));
+    assert.strictEqual(String(expr), 'CAST(AVG("bar") AS INTEGER)');
+    assert.strictEqual(expr.aggregate, 'AVG');
     assert.strictEqual(expr.column, 'bar');
     assert.deepStrictEqual(expr.columns, ['bar']);
-    assert.strictEqual(expr.label, 'sum(bar)');
+    assert.strictEqual(expr.label, 'avg(bar)');
   });
 });
