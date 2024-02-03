7b61bf6d44507dfeea7cb83cc2f559786f5592ff
Dominik Moritz <domoritz@gmail.com>
2024-02-03 11:18:26 -0500

    Untested implementation of bundling

--- packages/server/mosaic_server/__init__.py
+++ packages/server/mosaic_server/__init__.py
@@ -1,4 +1,6 @@
 import logging
+import os
+import re
 import sys
 import time
 
@@ -15,13 +17,15 @@ con = duckdb.connect()
 
 cache = dict()
 
+BUNDLE_DIR = ".mosaic/bundle"
+
 
 def retrieve(query, get):
     sql = query.get("sql")
-    type = query.get("type")
+    command = query.get("type")
     persist = query.get("persist", False)
 
-    key = (sql, type)
+    key = (sql, command)
     result = cache.get(key)
 
     if result:
@@ -46,37 +50,111 @@ def get_json(sql):
     return result.to_json(orient="records")
 
 
+def create_bundle(queries):
+    describe_re = re.compile(r"^DESCRIBE ")
+    pragma_re = re.compile(r"^PRAGMA ")
+    view_re = re.compile(r"^CREATE( TEMP| TEMPORARY)? VIEW")
+    table_re = re.compile(r"^CREATE( TEMP| TEMPORARY)? TABLE( IF NOT EXISTS)? ([^\s]+)")
+
+    manifest = {"tables": [], "queries": []}
+
+    for query in queries:
+        sql = query if isinstance(query, str) else query.get("sql")
+
+        if query.get("alias"):
+            table = query.get("alias")
+            file = os.path.join(BUNDLE_DIR, f"{table}.parquet")
+            con.execute(f"COPY ({sql}) TO '{file}' (FORMAT PARQUET)")
+            manifest["tables"].append(table)
+
+        elif sql.startswith("CREATE "):
+            if view_re.match(sql):
+                continue  # Ignore views
+
+            table_match = table_re.match(sql)
+            if table_match:
+                table = table_match.group(3)
+                file = os.path.join(BUNDLE_DIR, f"{table}.parquet")
+                con.execute(sql)
+                con.execute(f"COPY {table} TO '{file}' (FORMAT PARQUET)")
+                manifest["tables"].append(table)
+
+        elif not pragma_re.match(sql):
+            command = "json" if describe_re.match(sql) else "arrow"
+            key = (sql, command)
+            if command == "arrow":
+                get = get_arrow
+            elif command == "json":
+                get = get_json
+            else:
+                raise ValueError(f"Unknown command {command}")
+            result = retrieve(query, get)
+            with open(os.path.join(BUNDLE_DIR, key), "wb") as f:
+                f.write(result)
+            manifest["queries"].append(key)
+
+    with open(os.path.join(BUNDLE_DIR, "bundle.json"), "w") as f:
+        ujson.dump(manifest, f, indent=2)
+
+    return manifest
+
+
+def load_bundle():
+    with open(os.path.join(BUNDLE_DIR, "bundle.json")) as f:
+        manifest = ujson.load(f)
+
+    # Load precomputed query results into the cache
+    for key in manifest["queries"]:
+        file = os.path.join(BUNDLE_DIR, key)
+        json_file = os.path.splitext(file)[1] == ".json"
+        with open(file, "rb") as f:
+            data = f.read()
+            cache.set(key, ujson.loads(data) if json_file else data)
+
+    # Load precomputed temp tables into the database
+    for table in manifest["tables"]:
+        file = os.path.join(BUNDLE_DIR, f"{table}.parquet")
+        con.execute(f"CREATE TEMP TABLE IF NOT EXISTS {table} AS SELECT * FROM '{file}'")
+
+
 def ws_open(ws):
     logger.info("A WebSocket got connected!")
 
 
 def ws_message(ws, message, opcode):
     try:
-        data = ujson.loads(message)
+        query = ujson.loads(message)
     except Exception as e:
         logger.error(e)
         ok = ws.send({"error": str(e)}, OpCode.TEXT)
         return
 
-    logger.debug(f"{data=}")
+    logger.debug(f"{query=}")
 
     start = time.time()
 
-    sql = data["sql"]
-    type = data["type"]
+    sql = query["sql"]
+    command = query["type"]
 
     try:
-        if type == "exec":
+        if command == "exec":
             con.execute(sql)
             ok = ws.send({}, OpCode.TEXT)
-        elif type == "arrow":
-            buffer = retrieve(data, get_arrow)
+        elif command == "arrow":
+            buffer = retrieve(query, get_arrow)
             ok = ws.send(buffer, OpCode.BINARY)
-        elif type == "json":
-            json = retrieve(data, get_json)
+        elif command == "json":
+            json = retrieve(query, get_json)
             ok = ws.send(json, OpCode.TEXT)
+        elif command == "create-bundle":
+            pass
+            create_bundle(query.get("queries"))
+            ok = ws.send({}, OpCode.TEXT)
+        elif command == "load-bundle":
+            pass
+            ok = ws.send({}, OpCode.TEXT)
         else:
-            ok = ws.send({"error": f"Unknown command {type}"}, OpCode.TEXT)
+            raise ValueError(f"Unknown command {command}")
     except Exception as e:
         logger.error(e)
         ok = ws.send({"error": str(e)}, OpCode.TEXT)
