5ddeccd8abb23d22a6e034b2fa78c73c6741e8b9
jheer <jeffrey.heer@gmail.com>
2024-02-16 19:59:35 -0800

    test: Update NYC Taxi Rides spec.

--- specs/esm/nyc-taxi-rides.js
+++ specs/esm/nyc-taxi-rides.js
@@ -4,11 +4,13 @@ await vg.coordinator().exec([
   vg.loadExtension("spatial"),
   vg.loadParquet("rides", "https://uwdata.github.io/mosaic-datasets/data/nyc-rides-2010.parquet", {
   select: [
+  "pickup_datetime::TIMESTAMP AS datetime",
   "ST_Transform(ST_Point(pickup_latitude, pickup_longitude), 'EPSG:4326', 'ESRI:102718') AS pick",
   "ST_Transform(ST_Point(dropoff_latitude, dropoff_longitude), 'EPSG:4326', 'ESRI:102718') AS drop"
 ]
 }),
   `CREATE TEMP TABLE IF NOT EXISTS trips AS SELECT
+  (HOUR(datetime) + MINUTE(datetime)/60) AS time,
   ST_X(pick) AS px, ST_Y(pick) AS py,
   ST_X(drop) AS dx, ST_Y(drop) AS dy
 FROM rides`
@@ -16,57 +18,74 @@ FROM rides`
 
 const $filter = vg.Selection.crossfilter();
 
-const defaultAttributes = [
-  vg.width(335),
-  vg.height(550),
-  vg.margin(0),
-  vg.xAxis(null),
-  vg.yAxis(null),
-  vg.xDomain([975000, 1005000]),
-  vg.yDomain([190000, 240000]),
-  vg.colorScale("symlog")
-];
-
-export default vg.hconcat(
-  vg.plot(
-    vg.raster(
-      vg.from("trips", {filterBy: $filter}),
-      {x: "px", y: "py", binType: "normal", binWidth: 1, bandwidth: 0}
+export default vg.vconcat(
+  vg.hconcat(
+    vg.plot(
+      vg.raster(
+        vg.from("trips", {filterBy: $filter}),
+        {x: "px", y: "py", binType: "normal", binWidth: 1, bandwidth: 0}
+      ),
+      vg.intervalXY({as: $filter}),
+      vg.text(
+        [{label: "Taxi Pickups"}],
+        {
+          dx: 10,
+          dy: 10,
+          text: "label",
+          fill: "black",
+          fontSize: "1.2em",
+          frameAnchor: "top-left"
+        }
+      ),
+      vg.width(335),
+      vg.height(550),
+      vg.margin(0),
+      vg.xAxis(null),
+      vg.yAxis(null),
+      vg.xDomain([975000, 1005000]),
+      vg.yDomain([190000, 240000]),
+      vg.colorScale("symlog"),
+      vg.colorScheme("blues")
     ),
-    vg.intervalXY({as: $filter}),
-    vg.text(
-      [{label: "Taxi Pickups"}],
-      {
-        dx: 10,
-        dy: 10,
-        text: "label",
-        fill: "black",
-        fontSize: "1.2em",
-        frameAnchor: "top-left"
-      }
-    ),
-    ...defaultAttributes,
-    vg.colorScheme("blues")
+    vg.hspace(10),
+    vg.plot(
+      vg.raster(
+        vg.from("trips", {filterBy: $filter}),
+        {x: "dx", y: "dy", binType: "normal", binWidth: 1, bandwidth: 0}
+      ),
+      vg.intervalXY({as: $filter}),
+      vg.text(
+        [{label: "Taxi Dropoffs"}],
+        {
+          dx: 10,
+          dy: 10,
+          text: "label",
+          fill: "black",
+          fontSize: "1.2em",
+          frameAnchor: "top-left"
+        }
+      ),
+      vg.width(335),
+      vg.height(550),
+      vg.margin(0),
+      vg.xAxis(null),
+      vg.yAxis(null),
+      vg.xDomain([975000, 1005000]),
+      vg.yDomain([190000, 240000]),
+      vg.colorScale("symlog"),
+      vg.colorScheme("oranges")
+    )
   ),
-  vg.hspace(10),
+  vg.vspace(10),
   vg.plot(
-    vg.raster(
-      vg.from("trips", {filterBy: $filter}),
-      {x: "dx", y: "dy", binType: "normal", binWidth: 1, bandwidth: 0}
+    vg.rectY(
+      vg.from("trips"),
+      {x: vg.bin("time"), y: vg.count(), fill: "steelblue", inset: 0.5}
     ),
-    vg.intervalXY({as: $filter}),
-    vg.text(
-      [{label: "Taxi Dropoffs"}],
-      {
-        dx: 10,
-        dy: 10,
-        text: "label",
-        fill: "black",
-        fontSize: "1.2em",
-        frameAnchor: "top-left"
-      }
-    ),
-    ...defaultAttributes,
-    vg.colorScheme("oranges")
+    vg.intervalX({as: $filter}),
+    vg.yTickFormat("s"),
+    vg.xLabel("Pickup Hour →"),
+    vg.width(680),
+    vg.height(100)
   )
 );
