c392c76066c718e9ce047617484e2ba6e351ef80
jheer <jeffrey.heer@gmail.com>
2024-02-22 13:00:56 -0800

    chore: Use built-in DuckDB epoch_ms.

--- packages/sql/src/datetime.js
+++ packages/sql/src/datetime.js
@@ -2,8 +2,7 @@ import { sql } from './expression.js';
 import { asColumn } from './ref.js';
 
 export const epoch_ms = expr => {
-  const d = asColumn(expr);
-  return sql`(1000 * (epoch(${d}) - second(${d})) + millisecond(${d}))::DOUBLE`;
+  return sql`epoch_ms(${asColumn(expr)})`;
 };
 
 export const dateMonth = expr => {
