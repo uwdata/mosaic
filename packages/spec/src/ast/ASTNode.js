955db5b1619c42d005d0d5da0b18fa423870a214
Dominik Moritz <domoritz@gmail.com>
2024-04-02 12:53:23 -0400

    Fix type errors with ASTNode

--- packages/spec/src/ast/ASTNode.js
+++ packages/spec/src/ast/ASTNode.js
@@ -4,15 +4,27 @@ export class ASTNode {
     this.children = children;
   }
 
+  /**
+   * @returns {void}
+   */
   instantiate(/* ctx */) {
+    // @ts-ignore
     throw Error('instantiate not implemented');
   }
 
+  /**
+   * @returns {string} The for for the node.
+   */
   codegen(/* ctx */) {
+    // @ts-ignore
     return Error('codegen not implemented');
   }
 
+  /**
+   * @returns {object} The node as JSON.
+   */
   toJSON() {
+    // @ts-ignore
     return Error('toJSON not implemented');
   }
 }
