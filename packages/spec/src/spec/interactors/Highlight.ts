87b1b2688bc75c2cce8f9e6cc09b757dd1a4dccd
jheer <jeffrey.heer@gmail.com>
2024-04-03 09:38:33 -0700

    feat: Add spec interactor typings.

---
+++ packages/spec/src/spec/interactors/Highlight.ts
@@ -0,0 +1,37 @@
+import { ParamRef } from '../Param.js';
+
+export interface Highlight {
+  /**
+   * Highlight selected marks by deemphasizing the others.
+   */
+  select: 'highlight';
+  /**
+   * The input selection. Unselected marks are deemphasized.
+   */
+  by: ParamRef;
+  /**
+   * The overall opacity of deemphasized marks.
+   * By default the opacity is set to 0.2.
+   */
+  opacity?: number;
+  /**
+   * The fill opacity of deemphasized marks.
+   * By default the fill opacity is unchanged.
+   */
+  fillOpacity?: number;
+  /**
+   * The stroke opacity of deemphasized marks.
+   * By default the stroke opacity is unchanged.
+   */
+  strokeOpacity?: number;
+  /**
+   * The fill color of deemphasized marks.
+   * By default the fill is unchanged.
+   */
+  fill?: string;
+  /**
+   * The stroke color of deemphasized marks.
+   * By default the stroke is unchanged.
+   */
+  stroke?: string;
+}
