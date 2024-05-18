1e0d0990284c6c0e33973daabdf947328f313a1d
jheer <jeffrey.heer@gmail.com>
2024-05-18 12:02:11 -0700

    feat: Add linear regression 10m example.

---
+++ docs/examples/linear-regression-10m.md
@@ -0,0 +1,18 @@
+<script setup>
+  import { coordinator } from '@uwdata/vgplot';
+  coordinator().clear();
+</script>
+
+# Linear Regression 10M
+
+A linear regression plot predicting flight arrival delay based on the time of departure, over 10 million flight records. Regression computation is performed in the database, with optimized selection updates using data cube indexes. The area around a regression line shows a 95% confidence interval. Select a region to view regression results for a data subset.
+
+<Example spec="/specs/yaml/linear-regression-10m.yaml" />
+
+## Specification
+
+::: code-group
+<<< @/public/specs/esm/linear-regression-10m.js [JavaScript]
+<<< @/public/specs/yaml/linear-regression-10m.yaml [YAML]
+<<< @/public/specs/json/linear-regression-10m.json [JSON]
+:::
---
+++ specs/esm/linear-regression-10m.js
@@ -0,0 +1,27 @@
+import * as vg from "@uwdata/vgplot";
+
+await vg.coordinator().exec([
+  `CREATE TEMP TABLE IF NOT EXISTS flights10m AS SELECT GREATEST(-60, LEAST(ARR_DELAY, 180))::DOUBLE AS delay, DISTANCE AS distance, DEP_TIME AS time FROM 'https://uwdata.github.io/mosaic-datasets/data/flights-10m.parquet'`
+]);
+
+const $query = vg.Selection.intersect();
+
+export default vg.plot(
+  vg.raster(
+    vg.from("flights10m"),
+    {x: "time", y: "delay", pixelSize: 4, imageRendering: "pixelated"}
+  ),
+  vg.regressionY(
+    vg.from("flights10m"),
+    {x: "time", y: "delay", stroke: "gray"}
+  ),
+  vg.regressionY(
+    vg.from("flights10m", {filterBy: $query}),
+    {x: "time", y: "delay", stroke: "firebrick"}
+  ),
+  vg.intervalXY({as: $query, brush: {fillOpacity: 0, stroke: "currentColor"}}),
+  vg.xDomain([0, 24]),
+  vg.yDomain([-60, 180]),
+  vg.colorScale("symlog"),
+  vg.colorScheme("blues")
+);
