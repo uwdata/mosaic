a33d43be8bf19e59e56c465bed4f1a72884b366a
Dominik Moritz <domoritz@gmail.com>
2024-07-22 17:11:51 -0400

    test arrow response

--- packages/duckdb-server-rust/src/test.rs
+++ packages/duckdb-server-rust/src/test.rs
@@ -1,4 +1,9 @@
 use super::*;
+use arrow::{
+    array::{Int32Array, RecordBatch},
+    datatypes::{DataType, Field, Schema},
+    ipc::reader::FileReader,
+};
 use axum::{
     body::Body,
     http::{self, Request, StatusCode},
@@ -8,28 +13,28 @@ use serde_json::json;
 use tower::ServiceExt;
 
 #[tokio::test]
-async fn select_1_get() {
-    let app = app().unwrap();
+async fn select_1_get() -> Result<()> {
+    let app = app()?;
 
     let response = app
         .oneshot(
             Request::builder()
                 .uri("/?type=json&sql=SELECT%201%20as%20foo")
-                .body(Body::empty())
-                .unwrap(),
+                .body(Body::empty())?,
         )
-        .await
-        .unwrap();
+        .await?;
 
     assert_eq!(response.status(), StatusCode::OK);
 
-    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body = response.into_body().collect().await?.to_bytes();
     assert_eq!(&body[..], b"[{\"foo\":1}]");
+
+    Ok(())
 }
 
 #[tokio::test]
-async fn select_1_post() {
-    let app = app().unwrap();
+async fn select_1_post() -> Result<()> {
+    let app = app()?;
 
     let response = app
         .oneshot(
@@ -37,16 +42,49 @@ async fn select_1_post() {
                 .method(http::Method::POST)
                 .uri("/")
                 .header(http::header::CONTENT_TYPE, "application/json")
-                .body(Body::from(
-                    serde_json::to_vec(&json!({"type": "json", "sql": "select 1 as foo"})).unwrap(),
-                ))
-                .unwrap(),
+                .body(Body::from(serde_json::to_vec(
+                    &json!({"type": "json", "sql": "select 1 as foo"}),
+                )?))?,
         )
-        .await
-        .unwrap();
+        .await?;
 
     assert_eq!(response.status(), StatusCode::OK);
 
-    let body = response.into_body().collect().await.unwrap().to_bytes();
+    let body = response.into_body().collect().await?.to_bytes();
     assert_eq!(&body[..], b"[{\"foo\":1}]");
+
+    Ok(())
+}
+
+#[tokio::test]
+async fn query_arrow() -> Result<()> {
+    let app = app()?;
+
+    let response = app
+        .oneshot(
+            Request::builder()
+                .method(http::Method::POST)
+                .uri("/")
+                .header(http::header::CONTENT_TYPE, "application/json")
+                .body(Body::from(serde_json::to_vec(
+                    &json!({"type": "arrow", "sql": "select 1 as foo"}),
+                )?))?,
+        )
+        .await?;
+
+    assert_eq!(response.status(), StatusCode::OK);
+
+    let body = response.into_body().collect().await?;
+
+    let mut reader = FileReader::try_new(std::io::Cursor::new(body.to_bytes()), None)?;
+    let actual_batch = reader.next().unwrap();
+    let actual_batch = actual_batch?;
+
+    let schema = Arc::new(Schema::new(vec![Field::new("foo", DataType::Int32, true)]));
+    let foo_values = Int32Array::from(vec![1]);
+    let batch = RecordBatch::try_new(schema.clone(), vec![Arc::new(foo_values)])?;
+
+    assert_eq!(actual_batch, batch);
+
+    Ok(())
 }
