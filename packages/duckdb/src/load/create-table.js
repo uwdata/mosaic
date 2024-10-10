a2ca5198d760c7c838ad654eb236043ebb2c43ae
Dominik Moritz <domoritz@gmail.com>
2024-10-10 17:24:31 -0400

    feat: use load methods from sql package in duckdb server, add createSchema method

--- packages/duckdb/src/load/create-table.js
+++
@@ -1,6 +0,0 @@
-export function createTable(db, name, as, options = {}) {
-  const { temp, replace } = options;
-  const create = `CREATE${replace ? ' OR REPLACE' : ''}`;
-  const type = `${temp ? 'TEMP ' : ''}TABLE${replace ? '' : ' IF NOT EXISTS'}`;
-  return db.exec(`${create} ${type} ${name} AS ${as}`);
-}
