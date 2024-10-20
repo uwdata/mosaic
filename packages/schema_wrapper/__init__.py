8b8ea0032e0b0226f37f9879a765f721a3e2a9fd
Xinyue-Yang <86961126+Xinyue-Yang@users.noreply.github.com>
2024-10-19 23:00:59 -0400

    add test cases

--- packages/schema_wrapper/__init__.py
+++ packages/schema_wrapper/__init__.py
@@ -1,4 +1,4 @@
-from .generate_schema_wrapper import generate_schema_wrapper
-from .utils import jsonschema_to_python_types
+from .src.generate_schema_wrapper import generate_schema_wrapper
+from .src.utils import jsonschema_to_python_types
 
 __all__ = ['generate_schema_wrapper', 'jsonschema_to_python_types']
