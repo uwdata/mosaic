7e792bd0703225951331583e03055579e3183709
Hamza Pereira <adj-obj@protonmail.com>
2024-11-12 13:21:19 -0500

    Updated to use SchemaBase parent class

---
+++ packages/schema_wrapper/test/mini_test.py
@@ -0,0 +1,41 @@
+"""
+pytest packages/schema_wrapper/test/test.py
+"""
+import unittest
+from pathlib import Path
+import pytest
+from ..src.utils import to_dict_ours, _todict
+
+from ..generated_classes import *
+
+def test_weather_plot():
+    plot_spec = Plot(
+
+        plot = [PlotMark(Dot(mark="dot", data=PlotFrom(from_="weather", filterBy="$click"), x=ChannelValueSpec(ChannelValue({"dateMonthDay": "date"})), y=ChannelValueSpec(ChannelValue("temp_max")), fill="weather", r="precipitation"))],
+        xyDomain = "Fixed",
+        xTickFormat = "%b",
+        colorDomain = "$domain",
+        colorRange = "$colors",
+        rDomain = "Fixed",
+        rRange = [2, 10],
+        width = 800
+    )
+    print(_todict(plot_spec))
+
+def test_stock_plot():
+    plot_spec = Plot(
+        plot = [PlotMark(LineY(
+            mark="lineY", 
+            data=PlotFrom(from_="aapl"), 
+            x=ChannelValueSpec(ChannelValue("Date")), 
+            y=ChannelValueSpec(ChannelValue("Close"))
+        ))],
+        width = 680,
+        height = 200
+    )
+    print(_todict(plot_spec))
+
+if __name__ == '__main__':
+    test_weather_plot()
+    test_stock_plot()
+
