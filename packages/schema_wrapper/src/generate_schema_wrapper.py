798e7622fb66a9d931809833b02552dc9ef44272
Hamza Pereira <adj-obj@protonmail.com>
2024-11-11 22:16:20 -0500

    Corrected schema wrapper function

--- packages/schema_wrapper/src/generate_schema_wrapper.py
+++ packages/schema_wrapper/src/generate_schema_wrapper.py
@@ -11,29 +11,13 @@ from itertools import chain
 from pathlib import Path 
 from urllib import request
 import graphlib
+from .utils import get_valid_identifier, get_key_by_value, get_dependencies
 
 sys.path.insert(0, str(Path.cwd()))
-from tools.schemapi import CodeSnippet, SchemaInfo, codegen
-from tools.schemapi.utils import (
-    TypeAliasTracer,
-    get_valid_identifier,
-    indent_docstring,
-    resolve_references,
-    rst_parse,
-    rst_syntax_for_class,
-    ruff_format_py,
-    ruff_write_lint_format_str,
-    spell_literal,
-)
 
 SCHEMA_VERSION: Final = "v0.10.0"
 SCHEMA_URL_TEMPLATE: Final = "https://raw.githubusercontent.com/uwdata/mosaic/refs/heads/main/docs/public/schema/{version}.json"
-
-def get_key_by_value(dictionary, target_value):
-    for key, value in dictionary.items():
-        if value == target_value:
-            return key
-    return None
+KNOWN_PRIMITIVES = {"string": "str", "boolean": "bool", "number": "float", "object": "Dict[str, Any]"}
 
 def schema_url(version: str = SCHEMA_VERSION) -> str:
     return SCHEMA_URL_TEMPLATE.format(version=version)
@@ -49,43 +33,19 @@ def download_schemafile(
         raise ValueError(msg)
     return schemapath
 
-def get_dependencies(data) -> List[str]:
-    dependencies = []
-
-    if isinstance(data, dict):
-        if "$ref" in data:
-            refVal = data["$ref"]
-            dependencies.append(refVal.split('/')[-1])
-        
-        for key, value in data.items():
-            if key != "anyOf":
-                dependencies = dependencies + get_dependencies(value)
-    elif isinstance(data, list):
-        for item in data:
-            dependencies = dependencies + get_dependencies(item)
-
-    return dependencies
-
 def generate_class(class_name: str, class_schema: Dict[str, Any]) -> str:
-    #Hard coded this for now...?
-    imports = "from typing import Any, Union\n"
-
-
-    # Define a list of primitive types
-    # primitive_types = ['string', 'number', 'integer', 'boolean']
+    class_name = get_valid_identifier(class_name)
 
     # Check if the schema defines a simple type (like string, number) without properties
-    if 'type' in class_schema and 'properties' not in class_schema: #DISCUSS: Change this to not isinstance(class_schema, Iterable)
-        #assert not isinstance(class_schema, Iterable)
+    if 'type' in class_schema and 'properties' not in class_schema:
         return f"class {class_name}:\n    def __init__(self):\n        pass\n"
 
     # Check for '$ref' and handle it
     if '$ref' in class_schema:
         ref_class_name = class_schema['$ref'].split('/')[-1]
-        return f"\nclass {class_name}:\n    pass  # This is a reference to {ref_class_name}\n"
-
+        return f"\nclass {class_name}:\n    pass  # This is a reference to '{ref_class_name}'\n"
     if 'anyOf' in class_schema:
-        return generate_any_of_class(class_name, class_schema['anyOf'])
+            return generate_any_of_class(class_name, class_schema['anyOf'])
 
     # Extract properties and required fields
     properties = class_schema.get('properties', {})
@@ -95,50 +55,93 @@ def generate_class(class_name: str, class_schema: Dict[str, Any]) -> str:
     class_def += "    def __init__(self"
 
     # Generate __init__ method parameters
+    optional_params = []
+
+    # Ensuring all the property names are valid Python identifiers
+    valid_properties = {}
     for prop, prop_schema in properties.items():
-        type_hint = get_type_hint(prop_schema)
+        valid_prop = get_valid_identifier(prop)
+        valid_properties[valid_prop] = prop_schema
+
+    for prop, prop_schema in valid_properties.items():
+        if 'anyOf' in prop_schema:
+            # Handle anyOf case
+            type_hint = f"Union[{', '.join(get_type_hint(item) for item in prop_schema['anyOf'])}]"
+        else:
+            type_hint = get_type_hint(prop_schema)
+        
         if prop in required:
             # Required parameters should not have default values
             class_def += f", {prop}: {type_hint}"
         else:
-            # Optional parameters should have a default value of None
-            class_def += f", {prop}: {type_hint} = None"
+            # Ensure we add optional parameters last
+            optional_params.append((prop, type_hint))
+
+    for prop, type_hint in optional_params:
+        class_def += f", {prop}: {type_hint} = None"
 
     class_def += "):\n"
 
     # Generate attribute assignments in __init__
-    for prop in properties:
+    for prop in valid_properties:
         class_def += f"        self.{prop} = {prop}\n"
 
     return class_def
 
+def get_type_union(types: List[str]):
+    unique_types = list(set(types))
+    if len(unique_types) == 1:
+        return unique_types[0]
+
+    # Moving the potential "Any" to the end of the list
+    if "Any" in unique_types:
+        unique_types.remove("Any")
+        unique_types.append("Any")
+    return f'Union[{", ".join(unique_types)}]'
 
 def generate_any_of_class(class_name: str, any_of_schemas: List[Dict[str, Any]]) -> str:
     types = [get_type_hint(schema) for schema in any_of_schemas]
-    type_union = "Union[" + ", ".join(f'"{t}"' for t in types) + "]"  # Add quotes
-
+    type_union = get_type_union(types)
+    
     class_def = f"class {class_name}:\n"
     class_def += f"    def __init__(self, value: {type_union}):\n"
     class_def += "        self.value = value\n"
     
     return class_def
 
-
-    
-def get_type_hint(prop_schema: Dict[str, Any]) -> str:
+def get_type_hint(type_schema: Dict[str, Any]) -> str:
         """Get type hint for a property schema."""
-        if 'type' in prop_schema:
-            if prop_schema['type'] == 'string':
-                return 'str'
-            elif prop_schema['type'] == 'boolean':
-                return 'bool'
-            elif prop_schema['type'] == 'object':
-                return 'Dict[str, Any]'
-        elif 'anyOf' in prop_schema:
-            types = [get_type_hint(option) for option in prop_schema['anyOf']]
-            return f'Union[{", ".join(types)}]'
-        elif '$ref' in prop_schema:
-            return prop_schema['$ref'].split('/')[-1]  # Get the definition name
+        if 'items' in type_schema:
+            assert type_schema['type'] == 'array'
+            
+            items_schema = type_schema['items']
+            # items_schema contains the types which are stored in the list
+
+            datatype = get_type_hint(items_schema)
+            return f"List[{datatype}]"
+            
+        if 'type' in type_schema:
+            if isinstance(type_schema['type'], list):
+                types = []
+                for t in type_schema['type']:
+                    datatype = KNOWN_PRIMITIVES.get(t)
+                    if datatype == None:
+                        types.append('Any')
+                    else:
+                        types.append(datatype)
+                
+                return get_type_union(types)
+            else:
+                datatype = KNOWN_PRIMITIVES.get(type_schema['type'])
+                if datatype == None:
+                    return 'Any'
+                return datatype
+        elif 'anyOf' in type_schema:
+            types = [get_type_hint(option) for option in type_schema['anyOf']]
+            return get_type_union(types)
+        elif '$ref' in type_schema:
+            ref_class_name = type_schema['$ref'].split('/')[-1]
+            return f'"{ref_class_name}"'  
         return 'Any'
 
 def load_schema(schema_path: Path) -> dict:
@@ -152,18 +155,15 @@ def generate_schema_wrapper(schema_file: Path, output_file: Path) -> str:
     
     rootschema_definitions = rootschema.get("definitions", {})
     ts = graphlib.TopologicalSorter()
-    
+
     for name, schema in rootschema_definitions.items():
-        #print(name)
         dependencies = get_dependencies(schema)
         if dependencies:
-            #print(dependencies)
             ts.add(name, *dependencies)
         else:
             ts.add(name)
     
     class_order = list(ts.static_order())
-    #print(class_order)
 
     definitions: Dict[str, str] = {}
 
@@ -173,8 +173,8 @@ def generate_schema_wrapper(schema_file: Path, output_file: Path) -> str:
         definitions[name] = class_code
 
     generated_classes =  "\n\n".join(definitions.values())
-    generated_classes = "from typing import Any, Union\n\n" + generated_classes
-    #print(generated_classes)
+    generated_classes = "from typing import List, Dict, Any, Union\n\n" + generated_classes
+
 
     with open(output_file, 'w') as f:
         f.write(generated_classes)
@@ -183,21 +183,22 @@ def main():
     parser = argparse.ArgumentParser(
         prog="our_schema_generator", description="Generate the JSON schema for mosaic apps"
     )   
+    parser.add_argument("schema_file", help="Path to the JSON schema file")
+
     parser.add_argument(
         "--download", action="store_true", help="download the schema"
     )   
     args = parser.parse_args()
+    
     #vn = '.'.join(version.split(".")[:1]) #Not using this currently
-    fp = (Path(__file__).parent / ".." / 'tools').resolve()
-    schemapath = fp / "testingSchema.json"
-    schemafile = download_schemafile( 
+    schemapath = Path(args.schema_file).resolve()
+    schemapath = download_schemafile( 
         version=SCHEMA_VERSION,
         schemapath=schemapath,
         download = args.download
     )
 
-    schema_file = "tools/testingSchema.json"  # Update this path as needed
-    output_file = Path("tools/generated_classes.py")
+    output_file = Path("packages/schema_wrapper/generated_classes.py")
     generate_schema_wrapper(schemapath, output_file)
 
 # Main execution
