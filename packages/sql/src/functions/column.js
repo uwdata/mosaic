46312ea761beba2e427cb38351507003437a166a
jheer <jeffrey.heer@gmail.com>
2024-11-20 10:27:18 -0800

    refactor: Make colum param a subclass of column ref for nicer typing.

--- packages/sql/src/functions/column.js
+++ packages/sql/src/functions/column.js
@@ -1,5 +1,5 @@
 import { ColumnParamNode } from '../ast/column-param.js';
-import { ColumnRefNode } from '../ast/column-ref.js';
+import { ColumnNameRefNode } from '../ast/column-ref.js';
 import { ParamNode } from '../ast/param.js';
 import { TableRefNode } from '../ast/table-ref.js';
 import { asTableRef } from '../util/ast.js';
@@ -10,11 +10,11 @@ import { isParamLike } from '../util/type-check.js';
  * @param {string | import('../types.js').ParamLike} name The column name,
  *  as a string or as a dynamic parameter.
  * @param {string | string[] | TableRefNode} [table] The table reference.
- * @returns {ColumnRefNode}
+ * @returns {import('../ast/column-ref.js').ColumnRefNode}
  */
 export function column(name, table) {
   const tref = asTableRef(table);
   return isParamLike(name)
     ? new ColumnParamNode(new ParamNode(name), tref)
-    : new ColumnRefNode(name, tref);
+    : new ColumnNameRefNode(name, tref);
 }
