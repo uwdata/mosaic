b89617f99ca9b59efb4a07d031fa7217ce13797c
Oh-Hyun Kwon <oh@kwon.io>
2024-12-04 11:46:03 -0500

    fix typo

--- packages/core/src/util/js-type.js
+++ packages/core/src/util/js-type.js
@@ -2,7 +2,7 @@
  * Maps a SQL data type to its corresponding JavaScript type.
  * @param {string} type The name of a SQL data type
  * @returns The corresponding JavaScript type name
- * @throws {Error} Throws an error if the give SQL type name is unsupported or unrecognized.
+ * @throws {Error} Throws an error if the given SQL type name is unsupported or unrecognized.
  */
 export function jsType(type) {
   switch (type) {
