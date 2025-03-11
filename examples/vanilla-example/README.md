bdd3384a7e800b53739e018d49668f1a48f6156d
Dominik Moritz <domoritz@gmail.com>
2025-03-11 12:47:10 -0500

    docs: add simple vanilla example (#710)
    * docs: add simple vanilla example
    
    * chore: update lockfile
    
    * bump deps
    
    * add vite config

---
+++ examples/vanilla-example/README.md
@@ -0,0 +1,5 @@
+# Mosaic Example
+
+This example shows Mosaic embedded in a website.
+
+Run this example with `npm run dev`.
---
+++ examples/vanilla-example/src/main.js
@@ -0,0 +1,21 @@
+import { coordinator, wasmConnector } from '@uwdata/mosaic-core';
+import { loadParquet } from '@uwdata/mosaic-sql';
+import * as vg from '@uwdata/vgplot';
+
+const wasm = wasmConnector({ log: false });
+coordinator().databaseConnector(wasm);
+
+await vg.coordinator().exec([
+  loadParquet("aapl", `${window.location}stocks.parquet`, {where: "Symbol = 'AAPL'"})
+]);
+
+document.getElementById("plot").replaceChildren(
+  vg.plot(
+    vg.lineY(
+      vg.from("aapl"),
+      {x: "Date", y: "Close", tip: true}
+    ),
+    vg.width(680),
+    vg.height(200)
+  )
+)
