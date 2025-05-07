c11a62924c3b757beb4ea5c5418c74062bb80f6f
jheer <jeffrey.heer@gmail.com>
2025-05-07 14:55:50 -0700

    fix: Fix SQL rewrite, do not mutate input.

--- packages/sql/src/visit/rewrite.js
+++ packages/sql/src/visit/rewrite.js
@@ -4,7 +4,7 @@ import { recurse } from './recurse.js';
 
 /**
  * Rewrite a SQL expression, based on a map of nodes to replace.
- * This method updates the expression in place.
+ * This method copies nodes as needed; it does not modify the input node.
  * @param {ExprNode} node The root AST node of the expression.
  * @param {Map<ExprNode, ExprNode>} map The rewrite map.
  *  When encountered, key nodes are replaced by value nodes.
@@ -16,18 +16,30 @@ export function rewrite(node, map) {
   } else if (isNode(node)) {
     const props = recurse[node.type];
     const n = props?.length ?? 0;
-    for (let i = 0; i < n; ++i) {
-      const prop = props[i];
-      const child = node[prop];
-      if (Array.isArray(child)) {
-        const m = child.length;
-        for (let j = 0; j < m; ++j) {
-          child[j] = rewrite(child[j], map);
+    if (n > 0) {
+      node = cloneNode(node);
+      for (let i = 0; i < n; ++i) {
+        const prop = props[i];
+        const child = node[prop];
+        if (Array.isArray(child)) {
+          const m = child.length;
+          for (let j = 0; j < m; ++j) {
+            child[j] = rewrite(child[j], map);
+          }
+        } else if (child) {
+          node[prop] = rewrite(child, map);
         }
-      } else if (child) {
-        node[prop] = rewrite(child, map);
       }
     }
   }
   return node;
+}
+
+// TODO: consider typed node.clone() methods
+function cloneNode(node) {
+  const clone = new node.constructor();
+  for (const key in node) {
+    clone[key] = node[key];
+  }
+  return clone;
 }
