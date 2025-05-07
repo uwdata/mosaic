ee357b3fd377ee274ab89190f831134c71e06553
Jeffrey Heer <jeffrey.heer@gmail.com>
2025-05-07 10:57:08 -0700

    Expose Arrow IPC options for database connectors. (#766)
    * feat!: Add db connector ipc options.
    
    * fix: Fix ExtractionOptions type use.

--- packages/core/src/util/decode-ipc.js
+++ packages/core/src/util/decode-ipc.js
@@ -1,11 +1,15 @@
+/** @import { ExtractionOptions, Table } from '@uwdata/flechette' */
 import { tableFromIPC } from '@uwdata/flechette';
 
 /**
- * Decode Arrow IPC bytes to a table instance, with an option to map date and
- * timestamp values to JS Date objects.
+ * Decode Arrow IPC bytes to a table instance.
+ * The default options map date and timestamp values to JS Date objects.
  * @param {ArrayBuffer | Uint8Array} data Arrow IPC bytes.
- * @returns {import('@uwdata/flechette').Table} A table instance.
+ * @param {ExtractionOptions} [options] Arrow IPC extraction options.
+ *  If unspecified, the default options will extract date and timestamp
+ *  values to JS Date objects.
+ * @returns {Table} A table instance.
  */
-export function decodeIPC(data) {
-  return tableFromIPC(data, { useDate: true });
+export function decodeIPC(data, options = { useDate: true }) {
+  return tableFromIPC(data, options);
 }
