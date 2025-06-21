c96b3ce280beb4acc38647144c0c64e01b450432
jheer <jeffrey.heer@gmail.com>
2025-06-21 11:06:02 +0200

    fix: Fix more package paths.

--- vite.config.js
+++ vite.config.js
@@ -8,12 +8,12 @@ export default {
   resolve: {
     alias: {
       // Define aliases so that vite picks up the source files.
-      '@uwdata/mosaic-core': path.resolve(__dirname, './packages/core/src/index.ts'),
-      '@uwdata/mosaic-sql': path.resolve(__dirname, './packages/sql/src/index.ts'),
-      '@uwdata/vgplot': path.resolve(__dirname, './packages/vgplot/src/index.js'),
-      '@uwdata/mosaic-spec': path.resolve(__dirname, './packages/spec/src/index.js'),
-      '@uwdata/mosaic-inputs': path.resolve(__dirname, './packages/inputs/src/index.js'),
-      '@uwdata/mosaic-plot': path.resolve(__dirname, './packages/plot/src/index.js'),
+      '@uwdata/mosaic-core': path.resolve(__dirname, './packages/mosaic/core/src/index.ts'),
+      '@uwdata/mosaic-sql': path.resolve(__dirname, './packages/mosaic/sql/src/index.ts'),
+      '@uwdata/vgplot': path.resolve(__dirname, './packages/vgplot/vgplot/src/index.js'),
+      '@uwdata/mosaic-spec': path.resolve(__dirname, './packages/vgplot/spec/src/index.js'),
+      '@uwdata/mosaic-inputs': path.resolve(__dirname, './packages/vgplot/inputs/src/index.js'),
+      '@uwdata/mosaic-plot': path.resolve(__dirname, './packages/vgplot/plot/src/index.js'),
     }
   },
   test: {
