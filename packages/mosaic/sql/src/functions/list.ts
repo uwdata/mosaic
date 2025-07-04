0f059690c2825cc0f1352ddffc87fdec914ef113
Derek Perkins <derek@nozzle.io>
2025-07-03 20:22:23 -0600

    feat: add list_contains, list_has_all, and list_has_any

---
+++ packages/mosaic/sql/src/functions/list.ts
@@ -0,0 +1,46 @@
+import type {ExprValue, MaybeArray} from '../types.js';
+import { asLiteral } from '../util/ast.js';
+import { argsList, fn } from '../util/function.js';
+
+function listFn(name: string, expr: MaybeArray<ExprValue>, ...args: unknown[]) {
+  return fn(name, expr, ...(argsList(args).map(asLiteral)));
+}
+
+/**
+ * Function that returns true if the list contains the element,
+ * false otherwise.
+ * @param list
+ * @param element
+ */
+export function list_contains(
+  list: MaybeArray<ExprValue>,
+  element: ExprValue
+) {
+  return listFn('list_contains', list, element);
+}
+
+/**
+ * Function that returns true if all elements of sub-list exist in list,
+ * false otherwise.
+ * @param list
+ * @param sub_list
+ */
+export function list_has_all(
+  list: MaybeArray<ExprValue>,
+  sub_list: ExprValue[]
+) {
+  return listFn('list_has_all', list, sub_list);
+}
+
+/**
+ * Function that returns true if any elements exist in both lists,
+ * false otherwise.
+ * @param list1
+ * @param list2
+ */
+export function list_has_any(
+  list1: MaybeArray<ExprValue>,
+  list2: ExprValue[]
+) {
+  return listFn('list_has_any', list1, list2);
+}
---
+++ packages/mosaic/sql/test/list.test.ts
@@ -0,0 +1,30 @@
+import { expect, describe, it } from 'vitest';
+import {literal, column} from '../src/index.js';
+import { columns } from './util/columns.js';
+import {list_contains, list_has_all, list_has_any} from "../src/functions/list";
+
+describe('List functions', () => {
+  it('include list_contains', () => {
+    const expr = list_contains(column("foo"), literal('bar'))
+    expect(String(expr)).toBe(`list_contains("foo", 'bar')`);
+    expect(expr.name).toBe('list_contains');
+    expect(expr.args.length).toBe(2);
+    expect(columns(expr)).toStrictEqual(['foo']);
+  });
+
+  it('include list_has_all', () => {
+    const expr = list_has_all(column("foo"), [literal('bar'), literal('baz')])
+    expect(String(expr)).toBe(`list_has_all("foo", ['bar', 'baz'])`);
+    expect(expr.name).toBe('list_has_all');
+    expect(expr.args.length).toBe(2);
+    expect(columns(expr)).toStrictEqual(['foo']);
+  });
+
+  it('include list_has_any', () => {
+    const expr = list_has_any(column("foo"), [literal('bar'), literal('baz')])
+    expect(String(expr)).toBe(`list_has_any("foo", ['bar', 'baz'])`);
+    expect(expr.name).toBe('list_has_any');
+    expect(expr.args.length).toBe(2);
+    expect(columns(expr)).toStrictEqual(['foo']);
+  });
+});
