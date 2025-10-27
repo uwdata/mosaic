bfeda5750a76e790414e61124076c06120cdcfa9
jheer <jeffrey.heer@gmail.com>
2025-10-27 14:07:57 -0700

    feat: Add filterPushdown, export bin option types, loosen scale name types.

--- packages/mosaic/sql/src/transforms/filter-query.ts
+++ packages/mosaic/sql/src/transforms/filter-query.ts
@@ -1,33 +1,48 @@
 import type { FilterExpr } from '../types.js';
+import { FromClauseNode } from '../ast/from.js';
 import { isSelectQuery, type Query } from '../ast/query.js';
 import { isTableRef, type TableRefNode } from '../ast/table-ref.js';
+import { asTableRef } from '../util/ast.js';
 import { deepClone } from '../visit/clone.js';
 import { walk } from '../visit/walk.js';
-import { FromClauseNode } from '../ast/from.js';
+
+/**
+ * Perform filter pushdown on a query: clones the given query and adds a
+ * WHERE clause for the specified base table.
+ * @param query The query to clone and extend.
+ * @param table The base table as a table name or table reference node.
+ * @param filter The filter predicate expression to add.
+ */
+export function filterPushdown(
+  query: Query,
+  table: string | TableRefNode,
+  filter: FilterExpr
+) {
+  const clone = deepClone(query);
+  const tableRef = asTableRef(table)!;
+  walk(clone, (node) => {
+    if (
+      isSelectQuery(node) &&
+      node._from.length === 1 &&
+      node._from[0] instanceof FromClauseNode &&
+      isTableRef(node._from[0].expr) &&
+      arrayEquals(node._from[0].expr.table, tableRef.table)
+    ) {
+      node.where(filter);
+    }
+  });
+  return clone;
+}
 
 /**
  * Returns a generator function that clones the given query and adds
- * a WHERE clause for the specified table reference.
+ * a WHERE clause for the specified base table.
  * @param query The query to clone and extend.
- * @param tableRef The table to filter.
+ * @param table The base table as a table name or table reference node.
  * @returns The generator function.
  */
-export function filterQuery(query: Query, tableRef: TableRefNode) {
-  return (filter: FilterExpr) => {
-    const clone = deepClone(query);
-    walk(clone, (node) => {
-      if (
-        isSelectQuery(node) &&
-        node._from.length === 1 &&
-        node._from[0] instanceof FromClauseNode &&
-        isTableRef(node._from[0].expr) &&
-        arrayEquals(node._from[0].expr.table, tableRef.table)
-      ) {
-        node.where(filter);
-      }
-    });
-    return clone;
-  };
+export function filterQuery(query: Query, table: string | TableRefNode) {
+  return (filter: FilterExpr) => filterPushdown(query, table, filter);
 }
 
 function arrayEquals(a: unknown[], b: unknown[]) {
