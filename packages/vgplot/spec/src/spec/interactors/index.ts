a6bd4f1352b29282fb6f341031863017fe079353
Shahmir Varqha <sham9871@gmail.com>
2025-11-25 01:40:19 +0800

    create index.ts files and remove test

---
+++ packages/vgplot/spec/src/spec/interactors/index.ts
@@ -0,0 +1,8 @@
+export type * from "./BrushStyles.js";
+export type * from "./Highlight.js";
+export type * from "./Interval1D.js";
+export type * from "./Interval2D.js";
+export type * from "./Nearest.js";
+export type * from "./PanZoom.js";
+export type * from "./Region.js";
+export type * from "./Toggle.js";
