be72aa457a3476720da4bfa065d20d4a46addf7d
Karthik <karthik.anand5@hotmail.com>
2026-05-18 16:58:58 -0400

    removed extra warnings, unused imports

--- packages/vgplot/python/spec_classes/generated_classes.py
+++ packages/vgplot/python/spec_classes/generated_classes.py
@@ -1,6 +1,5 @@
 from typing import List, Dict, Any, Union
 from spec_classes.SchemaBase import SchemaBase
-from spec_classes.utils import revert_validation
 
 
 class AggregateExpression(SchemaBase):
