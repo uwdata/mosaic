59e277689b2d3e3f91fe137cbcb709ee185cebb4
jheer <jeffrey.heer@gmail.com>
2026-06-07 14:48:57 -0700

    test: Ensure use of pushed-down query in preagg test.

--- packages/mosaic/core/test/preaggregator.test.ts
+++ packages/mosaic/core/test/preaggregator.test.ts
@@ -1,6 +1,6 @@
 import { describe, it, expect } from 'vitest';
 import type { CreateQuery, ExprNode, FilterExpr } from "@uwdata/mosaic-sql";
-import { Query, add, argmax, argmin, avg, corr, count, covarPop, covariance, desc, filterPushdown, geomean, gt, isNotDistinct, literal, loadObjects, max, min, mul, product, regrAvgX, regrAvgY, regrCount, regrIntercept, regrR2, regrSXX, regrSXY, regrSYY, regrSlope, stddev, stddevPop, sum, varPop, variance } from '@uwdata/mosaic-sql';
+import { Query, add, argmax, argmin, avg, corr, count, covarPop, covariance, desc, filterPushdown, geomean, gt, isNotDistinct, literal, loadObjects, max, min, mul, neq, product, regrAvgX, regrAvgY, regrCount, regrIntercept, regrR2, regrSXX, regrSXY, regrSYY, regrSlope, stddev, stddevPop, sum, varPop, variance } from '@uwdata/mosaic-sql';
 import { Coordinator, Selection, SelectionClause } from '../src/index.js';
 import { NodeConnector } from './util/node-connector.js';
 import { TestClient } from './util/test-client.js';
@@ -198,7 +198,7 @@ describe('PreAggregator', () => {
   it('supports queries with column index references', async () => {
     const query = (predicate: FilterExpr = []) => {
       return Query.from('testData')
-        .select({ measure: avg("x"), dim: "dim" })
+        .select({ measure: avg('x'), dim: 'dim' })
         .groupby(literal(2))
         .where(predicate)
         .orderby(desc(literal(1)))
@@ -219,10 +219,13 @@ describe('PreAggregator', () => {
 
   it('supports queries with filter pushdown applied', async () => {
     const query = (predicate: FilterExpr = []) => {
+      // include a non-selective clause to ensure that we always have
+      // a "pushed-down" query input to preaggregation.
+      const filter = [neq('dim', literal('c')), predicate].flat();
       const q = Query.from('testData')
-        .select({ measure: avg("x"), dim: "dim" })
-        .groupby("dim");
-      return predicate ? filterPushdown(q, 'testData', predicate) : q;
+        .select({ measure: avg('x'), dim: 'dim' })
+        .groupby('dim');
+      return filterPushdown(q, 'testData', filter);
     };
     expect(await run(query)).toStrictEqual([3.5, true]);
   });
