b52e90f6d6f52d073c869b5336999a6b324155e0
Dominik Moritz <589034+domoritz@users.noreply.github.com>
2026-07-13 15:41:41 -0400

    Fix typo

--- packages/mosaic/core/src/util/positional.ts
+++ packages/mosaic/core/src/util/positional.ts
@@ -4,7 +4,7 @@ import type { ExprNode, LiteralNode, SelectClauseNode } from '@uwdata/mosaic-sql
  * Resolve a positional (1-based index) reference to a select clause entry.
  * Returns the corresponding select clause node if found, otherwise returns
  * undefined.
- * @param expr The SQL exprssion node to check.
+ * @param expr The SQL expression node to check.
  * @param select An array of select clause nodes.
  */
 export function resolvePositional(expr: ExprNode, select: SelectClauseNode[]) {
