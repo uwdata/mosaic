30c416e9d5b867ecaf56760c317269806fcc6612
Jeffrey Heer <jeffrey.heer@gmail.com>
2026-09-24 16:53:03 -0700

    feat!: Use lineage analysis for preaggregation, plus misc fixes. (#1263)
    - **Breaking**: remove `subqueries` and `cteFor` properties from `Query`
    instances.
    - **Breaking**: remove CSV, JSON, and Parquet load helpers from node
    duckdb server package. These methods were simply wrappers around the
    mosaic-sql equivalents. Removing them resolves a circular dependency.
    - Add proper lineage analysis to aid preaggregation: for finding base
    tables or base expressions and performing column pushdown. The result is
    more correct query analysis, including better recognition of
    non-optimizable queries.
    - Update sql `literal` to pad month and date values when serializing
    Date values.
    - Update Mosaic preaggregator for improved error reporting on table
    creation failure.
    - Add tests for SQL literal nodes.
    - Add tests for lineage helpers and preaggregation improvements.
    
    ---------
    
    Co-authored-by: Dominik Moritz <github@domoritz.de>
    Co-authored-by: Dominik Moritz <589034+domoritz@users.noreply.github.com>

--- packages/mosaic/sql/src/ast/literal.ts
+++ packages/mosaic/sql/src/ast/literal.ts
@@ -15,6 +15,10 @@ export class LiteralNode extends ExprNode {
   }
 }
 
+function padZero(value: unknown, len = 2) {
+  return `${value}`.padStart(len, '0');
+}
+
 export function literalToSQL(value: unknown) {
   switch (typeof value) {
     case 'number':
@@ -33,7 +37,7 @@ export function literalToSQL(value: unknown) {
         const m = value.getUTCMonth();
         const d = value.getUTCDate();
         return ts === Date.UTC(y, m, d)
-          ? `DATE '${y}-${m+1}-${d}'` // utc date
+          ? `DATE '${y}-${padZero(m+1)}-${padZero(d)}'` // utc date
           : `epoch_ms(${ts})`; // timestamp
       } else if (value instanceof RegExp) {
         return `'${value.source}'`;
--- packages/server/duckdb/src/load/parquet.js
+++
@@ -1,5 +0,0 @@
-import { loadParquet as loadParquetSQL } from '@uwdata/mosaic-sql';
-
-export function loadParquet(db, tableName, fileName, options = {}) {
-  return db.exec(loadParquetSQL(tableName, fileName, options));
-}
